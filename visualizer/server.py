39d1cc7e50ef4056e2810c6682d55e230e12309f
Jaime Cernuda <jcernudagarcia@hawk.iit.edu>
2023-10-28 18:29:32 -0500

    added arguments to control sleep time and the port

--- visualizer/server.py
+++ visualizer/server.py
@@ -4,6 +4,12 @@ from flask import Flask, render_template, jsonify, url_for
 import random
 from python.generator import generate_metadata
 import logging
+import argparse
+
+parser = argparse.ArgumentParser(description="Run a Flask application")
+parser.add_argument('--port', type=int, default=5000, help='Port to run the Flask application on')
+parser.add_argument('--sleep_time', type=float, default=0.5, help='Sleep time for the /metadata route')
+args = parser.parse_args()
 
 app = Flask(__name__)
 
@@ -16,7 +22,7 @@ def index():
 
 @app.route('/metadata')
 def get_metadata():
-    time.sleep(0.5)
+    time.sleep(args.sleep_time)
     metadata = generate_metadata(num_buckets=3, num_blobs=100, num_targets=4, num_nodes=16)
     return jsonify(metadata)
 
@@ -27,4 +33,4 @@ def get_metadata():
 #     return render_template('index.html', image_url1=grc_png, image_url2=grc_jpeg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=args.port, debug=True)
